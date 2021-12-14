0f5fa4899a73c4bc82765a0a92f6d16d1ae292a2
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2021-12-14 19:54:10 +0100

    all setup and ready to develop

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,22 +5,53 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    {
-      resolve: `gastby-source-filesystem`,
-      name: `pages`,
-      options: `${__dirname}/src/pages`
-    },
-    {
-      resolve: `gastby-source-filesystem`,
-      name: `posts`,
-      options: `${__dirname}/src/posts`
-    },
-    {
-      resolve: `gastby-source-filesystem`,
-      name: `images`,
-      options: `${__dirname}/src/images`
-    },
-  ],
+    /* Your site config here */
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/pages`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `posts`,
+                path: `${__dirname}/src/posts`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            },
+        },
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                extensions: [`.md`, `.mdx`],
+                gatsbyRemarkPlugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 1200,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: [
+                    `roboto mono`,
+                    `muli\:400,400i,700,700i`,
+                ],
+            },
+        },
+    ],
 }
---
+++ src/images/README.md
@@ -0,0 +1,4 @@
+---
+title: placeholder file 
+---
+This is a placeholder file so that we are able to commit all of the project directory tree to github
