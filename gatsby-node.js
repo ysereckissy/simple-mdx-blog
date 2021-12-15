3dde728bda27bf04caeef31fd23b1d444eeba065
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2021-12-15 14:28:44 +0100

    create file programmatically

---
+++ gatsby-node.js
@@ -0,0 +1,44 @@
+/*
+exports.createPages = async ({ actions, graphql }) => {
+    const data = await graphql`
+        query {
+            allMdx(sort: {fields: frontmatter___slug, order: DESC}) {
+                edges {
+                    node {
+                        frontmatter {
+                            slug
+                        }
+                        id
+                    }
+                }
+            }
+        }
+    `;
+    /// create paginated pages for posts
+    const postPerPage = 3;
+    const numPages = Math.ceil(data.allMdx.edges.length / postPerPage);
+    Array.from({ length: numPages }).forEach((_, index) => {
+        actions.createPages({
+            path: index === 0 ? `/` : `/${i + 1}`,
+            component: require.resolve("./src/templates/allPosts.js"),
+            context: {
+                limit: postPerPage,
+                skip: index * postPerPage,
+                numPages,
+                currentPage: index + 1,
+            }
+        })
+    });
+
+    /// Create single blog post
+    data.allMdx.edges.forEach(edge => {
+        const slug = edge.frontmatter.slug;
+        const id = edge.node.id;
+        actions.createPages({
+            path: slug,
+            component: require.resolve(`./src/templates/singlePost.js`),
+            context: { id }
+        })
+    })
+}
+*/
--- src/images/README.md
+++
@@ -1,4 +0,0 @@
----
-title: placeholder file 
----
-This is a placeholder file so that we are able to commit all of the project directory tree to github
