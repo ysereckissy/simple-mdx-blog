aecb06cfd2b2636dc555fdc7f3d071c6f88ba321
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2021-12-16 20:35:54 +0100

    lesson 15 completed

---
+++ src/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ContentWrapper } from '../elements';
+
+
+export const Content = ({ children }) => {
+    return (
+        <ContentWrapper>
+            {children}
+        </ContentWrapper>
+    );
+};
