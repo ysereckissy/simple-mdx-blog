eb55fbe9ec457dd3b8d51d4c2969ce953a9654b1
Yannick Sereckissy <yannick.sereckissy@gmail.com>
2021-12-16 22:17:13 +0100

    lesson 17 completed

--- src/elements/ContentElements.js
+++ src/elements/ContentElements.js
@@ -9,7 +9,7 @@ export const ContentWrapper = styled.main`
     box-shadow: ${props => props.theme.shadows.shadow1};
     z-index: 10;
 
-    @media ${props => props.theme.breakpoints.tablets} {
+    @media ${props => props.theme.breakpoints.tablet} {
         grid-column: 2 / span 6;
     }
 
